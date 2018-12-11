a69fb0d4da4c99cb33ddcd0770b30440ae4dd399
xiaowu351 <xiaowu351@126.com>
2018-12-11 19:45:59 +0800

    Xunit+Moq+MemoryEFDbContext测试UserController

---
+++ src/User.API.UnitTests/UserControllUnitTests.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using User.API.Controllers;
+using User.API.Data;
+using Xunit;
+
+namespace User.API.UnitTests
+{
+    public class UserControllUnitTests
+    {
+        private AppUserContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppUserContext>()
+                            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                            .Options;
+            var userContext = new AppUserContext(options);
+
+            userContext.AppUsers.Add(new Models.AppUser { Id = 1, Name = "test" });
+            userContext.SaveChanges();
+
+            return userContext;
+        }
+
+        [Fact]
+        public async Task Get_ReturnRightUser_With_ExpectedParameters()
+        {
+            var context = CreateDbContext();
+            var loggerMoq = new Mock<ILogger<UsersController>>();
+            var logger = loggerMoq.Object;
+
+            var controller = new UsersController(context, logger);
+
+            var response = await controller.Get();
+
+            Assert.IsType<JsonResult>(response);
+        }
+    }
+}
